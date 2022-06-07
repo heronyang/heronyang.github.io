1a90e359fa4e42adcf383c8a2d9730584a1a89a7
Heron Yang <heron.yang.tw@gmail.com>
2022-06-07 06:50:16 +0000

    Initialize the webpage.

---
+++ README.md
@@ -0,0 +1,13 @@
+# Heron's Knowledge
+
+## Install
+
+```
+$ ./setup.sh
+```
+
+## Run
+
+```
+$ sudo bundle3.0 exec jekyll serve -H 0.0.0.0 -P 4000
+```
---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+sudo apt update || exit 1
+sudo apt install -y ruby-full build-essential || exit 1
+sudo gem install jekyll bundler
+bundle add webrick
