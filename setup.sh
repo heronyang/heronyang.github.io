056c23fd3c3c79cba749e9b14ed35aa3c485e2c7
Heron Yang <heron.yang.tw@gmail.com>
2022-06-11 04:47:37 +0000

    Refine a bit.

--- setup.sh
+++ setup.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/bash
 sudo apt update || exit 1
 sudo apt install -y ruby-full build-essential || exit 1
-sudo gem install jekyll bundler
-bundle add webrick
+sudo gem install jekyll bundler github-pages
